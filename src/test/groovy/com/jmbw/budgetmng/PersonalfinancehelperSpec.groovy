e6ae2b2d03262dc66b39223a6c9d6c15005265c8
Arturo Jemmott <jmbwbox>
2022-04-11 15:36:40 +0000

    Initial commit

---
+++ src/test/groovy/com/jmbw/budgetmng/PersonalfinancehelperSpec.groovy
@@ -0,0 +1,19 @@
+package com.jmbw.budgetmng
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class PersonalfinancehelperSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
